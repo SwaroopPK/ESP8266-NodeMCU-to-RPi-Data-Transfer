33012ab2366f56b5bcafbd8dc3fd66d779979a25
SwaroopPK <47825893+SwaroopPK@users.noreply.github.com>
2019-07-07 19:16:39 +0530

    Update ESP_AP.ino

--- ESP8266_RPi MQTT/MQTT(esp_ap)/ESP_AP/ESP_AP.ino
+++ ESP8266_RPi MQTT/MQTT(esp_ap)/ESP_AP/ESP_AP.ino
@@ -5,17 +5,18 @@
 
 // WiFi
 // Make sure to update this for your own WiFi network!
-const char* ssid = "ESPAP_APPIKO";
-const char* wifi_password = "esp@appiko";
+// This is as per your convenience so that other devices may find the below SSID and connect using the below PASSWORD
+const char* ssid = "ESPAP_SSID";
+const char* wifi_password = "ESPAP_PASSWORD";
 
 // MQTT
 // Make sure to update this for your own MQTT Broker!
-const char* mqtt_server = "192.168.43.208";
-const char* mqtt_topic = "Voltage";
-const char* mqtt_username = "appiko";
-const char* mqtt_password = "appiko@123";
+const char* mqtt_server = "mqtt_server";
+const char* mqtt_topic = "Topic name";
+const char* mqtt_username = "Your mqtt_username";
+const char* mqtt_password = "Your mqtt_password";
 // The client id identifies the ESP8266 device. Think of it a bit like a hostname (Or just a name, like Greg).
-const char* clientID = "swaroop";
+const char* clientID = "Greg";
 
 
 // Initialise the WiFi and MQTT Client objects
@@ -49,7 +50,7 @@ void loop()
   voltage = analogRead(Pot_Pin);
   float V_pot = float(3)*voltage/1023;
   dtostrf(voltage,10,3,c);
-  if(mqttClient.publish("Appiko_Data/test", c))
+  if(mqttClient.publish(mqtt_topic, c))
   {
     Serial.println("Publish message success");
   }
